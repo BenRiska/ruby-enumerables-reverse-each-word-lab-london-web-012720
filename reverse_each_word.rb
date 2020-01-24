e1984c3be18dc933bc5a7928997ef88d28ccd3ed
Ben Taylor <ben10taylor10@hotmail.com>
2020-01-24 11:30:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,4 @@
 def reverse_each_word(str)
 
-output = ''
-array = str.split('')
-
-array.each do |n|
-n.reverse
-output += `#{n} `
-end
-output.strip
-output
+str.split(" ").map(&:reverse!).join(" ")
 end
